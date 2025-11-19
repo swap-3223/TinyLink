41d35f506d5efb7938c897f6365c86dd51e5c257
swap-3223 <waghswapnil@gmail.com>
2025-11-19 20:08:06 +0530

    fix api

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);app.use(express.json())
+app.use(cors());
 app.use('/api/links',router)
 
 app.get("/healthz", (req, res) => {
